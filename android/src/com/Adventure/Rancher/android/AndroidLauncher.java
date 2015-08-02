3e98ae991add7d0ac0a93db4930923e7f5cc65b2
atlasicus <matthew.m.drost@gmail.com>
2015-08-02 18:07:47 -0400

    Initial Commit

---
+++ android/src/com/Adventure/Rancher/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.Adventure.Rancher.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.Adventure.Rancher.AdventureRancher;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AdventureRancher(), config);
+	}
+}
